c1b9e2b377e236b70654e707c5f3fa60486186ed
Franco Ramiro  Gonzalez <91981585+francoGonzalez2706@users.noreply.github.com>
2024-07-09 13:34:24 -0300

    fijar el Allrelese y font list

--- src/Components/AllRelease/AllRelease.tsx
+++ src/Components/AllRelease/AllRelease.tsx
@@ -6,21 +6,24 @@ interface props {
 }
 export const AllRelease = ({ setSelected, selected }: props) => {
   return (
-    <div className="ListReleases">
-      <h2>All release notes</h2>
-      <div className="Container_Releses">
-        {json.release.map((real, index) => (
-          <div
-            className="relese"
-            style={{
-              fontSize: selected === index ? "1px solid black" : "",
-              color: selected === index ? "black" : "#7D879C",
-            }}
-            onClick={() => setSelected(index)}
-          >
-            {real.Title}:{real.subTitle}
-          </div>
-        ))}
+    <div>
+      <div className="ListReleases">
+        <h2>All release notes</h2>
+        <div className="Container_Releses">
+          {json.release.map((real, index) => (
+            <div
+              key={index}
+              className="relese"
+              style={{
+                fontSize: selected === index ? "1px solid black" : "",
+                color: selected === index ? "black" : "#7D879C",
+              }}
+              onClick={() => setSelected(index)}
+            >
+              {real.Title}:{real.subTitle}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
